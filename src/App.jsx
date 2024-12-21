bcfb265336c7f149855836ca3dfa6641a0197d7e
Nathan-Yennupiak <nathanyennupiak1@gmail.com>
2024-12-21 12:17:09 +0000

    Created moviecard Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
 const API_URL = " http://www.omdbapi.com?apikey=8191f3a7";
 
@@ -14,10 +15,12 @@ const movie1 = {
 };
 
 const App = () => {
+  const [movies, setMoviess] = useState([]);
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+    setMoviess(data.Search);
   };
 
   useEffect(() => {
@@ -35,22 +38,18 @@ const App = () => {
           onChange={() => {}}
         />
 
-        <img src={SearchIcon} alt="search" onChange={()=>{}} />
+        <img src={SearchIcon} alt="search" onChange={() => {}} />
       </div>
 
-      <div className="container">
-        <div className="movie">
-          <div>
-            <p>{movie1.Year}</p>
-          </div>
-
-          <div>
-            <img src={movie1.Poster !== 'N/A'?  movie1.Poster : 'https://via.placeholder.com/400' } alt={movie1.Title} />
-          </div>
-
+      {movies?.length > 0 ? (
+        <div className="container">
+          <MovieCard movie1={movie1} />
         </div>
-      </div>
-
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/MovieCard.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+
+const MovieCard = ({movie1}) => {
+  return (
+    <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+
+          <div>
+            <img src={movie1.Poster !== 'N/A'?  movie1.Poster : 'https://via.placeholder.com/400' } alt={movie1.Title} />
+          </div>
+
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+
+        </div>
+  )
+}
+
+MovieCard.propTypes = {
+  movie1: PropTypes.shape({
+    Year: PropTypes.string,
+    Poster: PropTypes.string,
+    Type: PropTypes.string,
+    Title: PropTypes.string
+  }).isRequired
+};
+
+
+export default MovieCard
